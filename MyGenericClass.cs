4465780e8cb671c27aa39dc1837801bb2adb45fc
Chinh-NTC <chinh251106@gmail.com>
2026-03-02 22:47:58 +0700

    Nop bai tap

---
+++ MyGenericClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BaiTapGeneric
+{
+    public class MyGenericClass<T>
+    {
+        public static void Display(T[] inputArray)
+        {
+            foreach (T element in inputArray)
+            {
+                Console.Write(element + "  ");
+            }
+            Console.WriteLine();
+        }
+
+        public static T[] Sort(T[] inputArray)
+        {
+            return inputArray;
+        }
+    }
+}
