1a823a16630010127ddb28bdc11051254602232f
Chinh-NTC <chinh251106@gmail.com>
2026-03-13 22:27:13 +0700

    Phần 1 bài 5 bình phương các số

---
+++ BT_LINQ/Phan1/Bai5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_LINQ_Bai5
+{
+    internal class Bai5
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+            Console.WriteLine("Danh sách ban đầu: ");
+            foreach (int i in numbers)
+            {
+                Console.Write(i + " ");
+            }
+
+            var numbers_BinhPhuong = numbers.Select(n => n*n).ToList();
+            Console.WriteLine("\nBài 5:");
+            Console.WriteLine("Danh sách sau khi bình phương: ");
+            foreach(var  i in numbers_BinhPhuong)
+            {
+                Console.Write(i + " ");
+            }    
+
+            Console.ReadKey();
+        }
+    }
+}
