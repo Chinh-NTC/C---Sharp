53c036d6ae1f6e28d96a2876f455204909ff09d9
Chinh-NTC <chinh251106@gmail.com>
2026-03-13 23:04:01 +0700

    Phần 2 bài 8 lọc sinh viên giỏi

---
+++ BT_LINQ/Phan2/Bai8.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_LINQ_Bai8
+{
+    internal class Bai8
+    {
+        class Student
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public double Score { get; set; }
+        }
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.OutputEncoding = Encoding.UTF8;
+
+                List<Student> students = new List<Student>()
+                {
+                new Student { Id = 1, Name = "An", Score = 8 },
+                new Student { Id = 2, Name = "Bình", Score = 6 },
+                new Student { Id = 3, Name = "Chi", Score = 9 },
+                new Student { Id = 4, Name = "Dũng", Score = 7 }
+                };
+
+                Console.WriteLine("=== DANH SÁCH TẤT CẢ SINH VIÊN ===");
+                foreach (var sv in students)
+                {
+                    Console.WriteLine($"ID: {sv.Id} | Tên: {sv.Name} | Điểm: {sv.Score}");
+                }
+                var sinhVienGioi = students.Where(s => s.Score >= 8).ToList();
+
+                Console.WriteLine("\n=== DANH SÁCH SINH VIÊN GIỎI (ĐIỂM >= 8) ===");
+                foreach (var sv in sinhVienGioi)
+                {
+                    Console.WriteLine($"ID: {sv.Id} | Tên: {sv.Name} | Điểm: {sv.Score}");
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
