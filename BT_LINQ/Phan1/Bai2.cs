4bf6f7b13e3b98540d09d8981055008d9053e9e3
Chinh-NTC <chinh251106@gmail.com>
2026-03-13 22:06:38 +0700

    Phần 1 bài 2 lọc số lớn hơn 5

---
+++ BT_LINQ/Phan1/Bai2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_LINQ_Bai2
+{
+    internal class Bai2
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7 , 8};
+            Console.WriteLine("Danh sách ban đầu: ");
+            foreach(int i in numbers)
+            {
+                Console.Write(i + " ");
+            }
+
+            var kt = numbers.Where(n => n > 5).ToList();
+            Console.WriteLine("\nBài 2:");
+            Console.WriteLine("Danh sách các số lớn hơn 5: ");
+            foreach(var i in kt)
+            {
+                Console.Write(i + " ");
+            }    
+
+            Console.ReadKey();
+        }
+    }
+}
