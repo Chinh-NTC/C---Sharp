a1760f343072ce704ad858d4c4937aee8fdb9d24
Chinh-NTC <chinh251106@gmail.com>
2026-03-13 22:19:23 +0700

    Phần 1 bài 4 sắp xếp giảm dần

---
+++ BT_LINQ/Phan1/Bai4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_LINQ_Bai4
+{
+    internal class Bai4
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+            Console.WriteLine("Danh sách ban đầu: ");
+            foreach (int i in numbers)
+            {
+                Console.Write(i + " ");
+            }
+
+            var sapXepTangDan = numbers.OrderByDescending(x => x).ToList();
+            Console.WriteLine("\nBài 3:");
+            Console.WriteLine("Danh sách giảm dần: ");
+            foreach (var i in sapXepTangDan)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
