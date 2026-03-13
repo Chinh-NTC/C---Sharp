c87089accfcf69d89f4ab6a8b59b1ef94b9b1553
Chinh-NTC <chinh251106@gmail.com>
2026-03-14 00:27:08 +0700

    Phần 3 bài 19 tìm số xuất hiện nhiều nhất

---
+++ BT_LINQ/Phan3/Bai19.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_LINQ_Bai19
+{
+    internal class Bai19
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            List<int> numbers = new List<int> { 1, 2, 2, 2, 3, 4, 4, 5 };
+
+            Console.WriteLine("Danh sách ban đầu: ");
+            foreach (var n in numbers)
+            {
+                Console.Write(n + " ");
+            }
+
+            // Nhóm các số trùng nhau thành 1 nhóm 
+            // Sắp xếp các nhóm giảm dần theo số lượng 
+            // Lấy nhóm đầu tiên
+            var numbers_laplaitop1 = numbers.GroupBy(n => n).OrderByDescending(d => d.Count()).FirstOrDefault();
+
+            Console.WriteLine("\nBài 19: ");
+            Console.WriteLine("Tìm số xuất hiện nhiều nhất: ");
+            if(numbers_laplaitop1 != null)
+                Console.WriteLine("Số {0} xuất hiện nhiều lần nhất với {1} lần xuất hiện.", numbers_laplaitop1.Key, numbers_laplaitop1.Count());
+        
+            Console.ReadKey();
+        }
+    }
+}
