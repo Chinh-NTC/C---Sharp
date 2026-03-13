fecab6d4b8179b270d6b8a202e57463351ce16b9
Chinh-NTC <chinh251106@gmail.com>
2026-03-13 23:18:00 +0700

    Phần 2 bài 10 sắp xếp sinh viên giảm dần theo điểm

---
+++ BT_LINQ/Phan2/Bai10.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_LINQ_Bai10
+{
+    internal class Bai10
+    {
+        class Student
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public double Score { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            List<Student> students = new List<Student>()
+            {
+            new Student { Id = 1, Name = "An", Score = 8 },
+            new Student { Id = 2, Name = "Bình", Score = 6 },
+            new Student { Id = 3, Name = "Chi", Score = 9 },
+            new Student { Id = 4, Name = "Dũng", Score = 7 }
+            };
+
+            Console.WriteLine("=== DANH SÁCH TẤT CẢ SINH VIÊN ===");
+            foreach (var sv in students)
+            {
+                Console.WriteLine($"ID: {sv.Id} | Tên: {sv.Name} | Điểm: {sv.Score}");
+            }
+
+            var sapXepDiemGiamDan = students.OrderByDescending( x => x.Score ).ToList();
+            Console.WriteLine("\nBài 10: ");
+            Console.WriteLine("DANH SÁCH SINH VIÊN GIẢM DẦN THEO ĐIỂM: ");
+            foreach(var sv in  sapXepDiemGiamDan)
+            {
+                Console.WriteLine($"ID: {sv.Id} | Tên: {sv.Name} | Điểm: {sv.Score}");
+            }    
+
+            Console.ReadKey();
+        }
+    }
+}
