974ddbf92ff78da0dd660480df1a0d2c274f3ae7
Chinh-NTC <chinh251106@gmail.com>
2026-03-13 22:29:36 +0700

    Phần 1 bài 6 đếm số chẵn

---
+++ BT_LINQ/Phan1/Bai6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_LINQ_Bai6
+{
+    internal class Bai6
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            Console.WriteLine("Danh sách ban đầu: ");
+            foreach (int i in numbers)
+            {
+                Console.Write(i + " ");
+            }
+
+        }
+    }
+}
