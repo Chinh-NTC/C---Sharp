64a347ef1b4cf608b52036caecb4ad91c54801d1
Chinh-NTC <chinh251106@gmail.com>
2026-03-14 12:15:33 +0700

    Phần 2 bài 9 lấy danh sách tên sinh viên

--- BT_LINQ/Phan2/Bai9.cs
+++ BT_LINQ/Phan2/Bai9.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Tên Nguyễn Tấn Chinh
+// MSV: 2415053122301
+// Lớp: 225LTC01
 namespace BT_LINQ_Bai9
 {
     internal class Bai9
@@ -34,6 +37,7 @@ namespace BT_LINQ_Bai9
             }
 
             var ds_tenSv = students.Select(n => n.Name).ToList();
+            Console.WriteLine("\nBài 9: ");
             Console.WriteLine("=== DANH SÁCH TÊN CÁC SINH VIÊN ===");
             foreach(var sv in ds_tenSv)
             {
