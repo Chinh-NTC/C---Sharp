090972d8b6dd79ffcb93ba00748bd021245a5425
Chinh-NTC <chinh251106@gmail.com>
2026-03-18 19:12:26 +0700

    Bài 6 kiểm tra phần tử tồn tại

---
+++ BT_Generic_collection/Phan1/Bai6.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Tên: Nguyễn Tấn Chinh
+// MSV: 2415053122301
+// Lớp:: 225LTC01
+namespace BT_Generic_Bai6
+{
+    internal class Bai6
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            List<string> danhSach = new List<string> { "C#", "Java", "Python", "JS", "SQL" };
+
+            Console.WriteLine("Danh sách hiện tại: " + string.Join(", ", danhSach));
+
+            Console.Write("\nNhập tên ngôn ngữ bạn muốn tìm: ");
+            string tuKhoa = Console.ReadLine();
+            bool timThay = danhSach.Contains(tuKhoa);
+            if (timThay)
+                Console.WriteLine($"==> Kết quả: '{tuKhoa}' CÓ tồn tại trong danh sách.");
+            else
+                Console.WriteLine($"==> Kết quả: '{tuKhoa}' KHÔNG tồn tại trong danh sách.");
+
+            Console.ReadKey();
+        }
+    }
+}
